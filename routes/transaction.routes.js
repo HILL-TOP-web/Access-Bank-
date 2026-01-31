e2730caeb94f4340def79b68162bad2a5de74b6d
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-01-31 23:35:10 +0100

    Create transaction.routes.js
    Yes

---
+++ routes/transaction.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+  getTransactions,
+  getTransactionById
+} from '../controllers/transaction.controller.js';
+
+import authMiddleware from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.get('/', authMiddleware, getTransactions);
+router.get('/:id', authMiddleware, getTransactionById);
+
+export default router;
