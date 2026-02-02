a768a1ea8dabefc494e10d23dee90626eb5da33b
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-02-02 20:54:26 +0100

    Create user.controller.js
    Yes

---
+++ controllers/user.controller.js
@@ -0,0 +1,26 @@
+import User from '../models/User.js';
+
+export const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const updates = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      updates,
+      { new: true }
+    ).select('-password');
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
