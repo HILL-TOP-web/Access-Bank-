19e83b3b8b1945af6a9e9f6663846000e6d13cab
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-02-07 20:56:13 +0100

    Add success and error response utility functions
    Yes

---
+++ utils/response.js
@@ -0,0 +1,27 @@
+// utils/response.js
+
+export const successResponse = (
+  res,
+  message,
+  data = {},
+  statusCode = 200
+) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data
+  });
+};
+
+export const errorResponse = (
+  res,
+  message,
+  statusCode = 400,
+  errors = null
+) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errors
+  });
+};
